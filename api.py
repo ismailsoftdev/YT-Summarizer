3e0b7183c8d5c1628a034cd25a76f063cfda9030
ismailsoftdev <ismailibrahimabdinur143@gmail.com>
2023-06-16 22:12:05 +0300

    Added comments to the api

--- api.py
+++ api.py
@@ -1,21 +1,39 @@
+# Import the necessary libraries.
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from transformers import pipeline
 from youtube_transcript_api import YouTubeTranscriptApi
 
+# Create a Flask app.
 app = Flask(__name__)
+
+# Allow cross-origin requests.
 CORS(app)
+
+# Define the `summarize_video()` function.
 
 
 @app.route('/summarize', methods=['POST'])
 def summarize_video():
+
+    # Get the video URL from the request body.
     youtube_video = request.json.get('video_url')
+
+    # Get the video ID from the video URL.
     video_id = youtube_video.split("=")[1]
+
+    # Get the transcript for the video.
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+    # Combine all of the transcript sentences into a single string.
     result = ""
     for i in transcript:
         result += i['text']
+
+    # Create a summarization pipeline.
     summarizer = pipeline("summarization")
+
+    # Summarize the transcript and limit the output to 300 tokens.
     num_iters = int(len(result) / 1000)
     summarized_text = []
     for i in range(0, num_iters + 1):
@@ -25,8 +43,11 @@ def summarize_video():
         out = out[0]
         out = out['summary_text']
         summarized_text.append(out)
+
+    # Return the summary as a JSON object.
     return jsonify({'summary': summarized_text})
 
 
+# Run the app.
 if __name__ == '__main__':
     app.run(debug=True)
